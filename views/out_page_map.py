30125e7a724e783bf8b6db92a4f96a25c8e63250
Fatih Tasdemir <tsdmrfth@gmail.com>
2020-05-03 00:10:14 +0300

    Updated out pages.

--- views/out_page_map.py
+++ views/out_page_map.py
@@ -2,7 +2,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import numpy as np
 import plotly.graph_objects as go
-import plotly_express as px
+import plotly.express as px
 from dash.dependencies import Input, Output
 from flask import session
 
@@ -15,32 +15,57 @@ def get_layout():
     years = session.get('years') if session.get('years') else [0]
     i_options = np.array(['country', 'sex', 'age', 'rel_area'])
     v_options = np.array(['animals', 'env', 'faith', 'health', 'resources', 'sort'])
+
     layout = html.Div([
-        dcc.Dropdown(
-            id='i-dropdown',
-            options=[{'label': i, 'value': i} for i in i_options],
-            value='sex'
-        ),
-        dcc.Dropdown(
-            id='v-dropdown',
-            options=[{'label': i, 'value': i} for i in v_options],
-            value='health'
-        ),
-        dcc.Dropdown(
-            id='i2-dropdown',
-            options=[{'label': i, 'value': i} for i in i_options],
-            value='age'
-        ),
+        html.Div([
+            html.Div([
+                html.Label('Measurement Indicators'),
+                dcc.Dropdown(
+                    id='v-dropdown',
+                    options=[{'label': i, 'value': i} for i in v_options],
+                    value='health'
+                ),
+                dcc.Dropdown(
+                    id='v2-dropdown',
+                    options=[{'label': i, 'value': i} for i in v_options],
+                    value='resources'
+                ),
+            ],
+                style={'width': '49%', 'display': 'inline-block'}),
+
+            html.Div([
+                html.Label('Categorical Indicators'),
+                dcc.Dropdown(
+                    id='i-dropdown',
+                    options=[{'label': i, 'value': i} for i in i_options],
+                    value='sex'
+                ),
+                dcc.Dropdown(
+                    id='i2-dropdown',
+                    options=[{'label': i, 'value': i} for i in i_options],
+                    value='age'
+                ),
+
+            ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
+        ], style={
+            'borderBottom': 'thin lightgrey solid',
+            'backgroundColor': 'rgb(250, 250, 250)',
+            'padding': '10px 5px'
+        }),
+
         html.Div([
             dcc.Graph(
                 id='crossfilter-map',
-                hoverData={'points': [{'customdata': 'BEL'}]}
-            )
+                clickData={'points': [{'hovertext': 'BEL'}]}
+            ),
+            dcc.Graph(id='contour-plot'),
         ], style={'width': '49%', 'display': 'inline-block', 'vertical-align': 'top'}),
 
         html.Div([
             dcc.Graph(id='vio-plot'),
             dcc.Graph(id='par-plot'),
+            dcc.Graph(id='area-plot'),
+
         ], style={'display': 'inline-block', 'width': '49%'}),
 
         dcc.Slider(
@@ -69,16 +94,18 @@ def update_map_graph(var, year_value=2010):
             year = item['year']
             table_name = item['table_name']
             dff = getTableDf(datas, year, table_name)[1]
+            hovertext = dff['country']
             fig = go.Figure(data=go.Choropleth(
                 locations=dff['country'],
                 z=dff[var],
-                text=dff['country'],
-                customdata=dff['country'],
+                # text = dff['country'],
+                hovertext=dff['country'],
                 colorscale='Blues',
                 autocolorscale=False,
                 reversescale=True,
                 marker_line_color='darkgray',
                 marker_line_width=0.5,
+                # colorbar_tickprefix = '$',
                 colorbar_title='Indicator',
             ))
 
@@ -106,18 +133,19 @@ def update_map_graph(var, year_value=2010):
 
 @app.callback(
     Output('vio-plot', 'figure'),
-    [Input('crossfilter-map', 'hoverData'), Input('i-dropdown', 'value'), Input('i2-dropdown', 'value'),
+    [Input('crossfilter-map', 'clickData'), Input('i-dropdown', 'value'), Input('i2-dropdown', 'value'),
      Input('v-dropdown', 'value'), Input('year--slider', 'value')])
-def update_vio(hoverData, ind, ind2, var, year_value):
+def update_vio(clickData, ind, ind2, var, year_value):
     all_data = session.get('all_data')
 
     for item in all_data:
         if item['year'] == year_value:
-            country_name = hoverData['points'][0]['customdata']
+            country_name = clickData['points'][0]['hovertext']
             datas = item['datas']
             year = item['year']
             table_name = item['table_name']
             dff = getTableDf(datas, year, table_name)[2]
+            dff = dff[dff['country'] == country_name].dropna()
             fig = px.violin(dff, x=dff[ind], y=dff[var], color=dff[ind2], box=True, violinmode='overlay', hover_data=dff.columns)
             xx = dff.groupby(ind).agg(['mean', 'median', 'std', 'skew', pd.Series.kurt])[var]
             li = xx.columns.tolist()
@@ -146,17 +174,63 @@ def update_vio(hoverData, ind, ind2, var, year_value):
 
 @app.callback(
     Output('par-plot', 'figure'),
-    [Input('crossfilter-map', 'hoverData'), Input('i-dropdown', 'value'), Input('i2-dropdown', 'value'),
+    [Input('crossfilter-map', 'clickData'), Input('i-dropdown', 'value'), Input('i2-dropdown', 'value'),
      Input('v-dropdown', 'value'), Input('year--slider', 'value')])
-def update_par(hoverData, ind, ind2, var, year_value):
+def update_par(clickData, ind, ind2, var, year_value):
     all_data = session.get('all_data')
-    
+
     for item in all_data:
         if item['year'] == year_value:
+            country_name = clickData['points'][0]['hovertext']
             datas = item['datas']
             year = item['year']
             table_name = item['table_name']
             dff = getTableDf(datas, year, table_name)[2]
-            df = dff[[ind, ind2, var]]
-            fig = px.parallel_categories(df, color=var)
+            dff = dff[dff['country'] == country_name].dropna()
+            # df = dff[[ind, ind2, var]]
+            fig = px.parallel_categories(dff, dimensions=[ind, ind2, var], color=var)
+            return fig
+
+
+@app.callback(
+    Output('area-plot', 'figure'),
+    [Input('crossfilter-map', 'clickData'), Input('i-dropdown', 'value'), Input('i2-dropdown', 'value'),
+     Input('v-dropdown', 'value'), Input('year--slider', 'value')])
+def update_are(clickData, ind, ind2, var, year_value):
+    all_data = session.get('all_data')
+
+    for item in all_data:
+        if item['year'] == year_value:
+            country_name = clickData['points'][0]['hovertext']
+            datas = item['datas']
+            year = item['year']
+            table_name = item['table_name']
+            dff = getTableDf(datas, year, table_name)[2]
+            dff = dff[dff['country'] == country_name].dropna()
+            fig = px.area(dff, x=ind, y=var, color=ind2)
+            fig['layout']['yaxis1'].update(title='', range=[0, 10], autorange=False)
+            return fig
+
+
+@app.callback(
+    Output('contour-plot', 'figure'),
+    [Input('crossfilter-map', 'clickData'), Input('v-dropdown', 'value'), Input('v2-dropdown', 'value'),
+     Input('year--slider', 'value')])
+def update_con(clickData, v1, v2, year_value):
+    all_data = session.get('all_data')
+
+    for item in all_data:
+        if item['year'] == year_value:
+            country_name = clickData['points'][0]['hovertext']
+            datas = item['datas']
+            year = item['year']
+            table_name = item['table_name']
+            dff = getTableDf(datas, year, table_name)[2]
+            dff = dff[dff['country'] == country_name].dropna()
+            fig = px.scatter(dff, x=v1, y=v2, trendline="ols", marginal_y="histogram", marginal_x="histogram")
+            trace2 = go.Histogram2dContour(
+                x=dff[v1], y=dff[v2], name='density', ncontours=20,
+                colorscale='Hot', reversescale=True, showscale=False, hoverinfo='skip'
+            )
+            fig.add_trace(trace2)
             return fig
--- views/out_page_scatter.py
+++ views/out_page_scatter.py
@@ -7,74 +7,63 @@ from flask import session
 from db_mgt import getTableDf
 from server import app
 
+import plotly.express as px
+import numpy as np
+import plotly.figure_factory as ff
+
+v_options = np.array(['animals', 'env', 'faith', 'health', 'resources', 'sort'])
+
 
 def getLayout():
     years = session.get('years') if session.get('years') else [0]
     layout = html.Div([
         dcc.Location(id='out_page_url', refresh=True),
-        dbc.Row([
-            dbc.Col([
-                dbc.Row([
-                    dbc.Col([
-                        # html.H4(session.get('year'))
-                    ]),
-                    dbc.Col([
-                        dbc.Button("Go Back", id="go_back")
-                    ], className="text-right")
-                ], className="mb-4"),
+        html.Div([
+            html.Div([
 
                 html.Div([
-                    html.Div([
-                        dcc.Dropdown(
-                            id='crossfilter-xaxis-column',
-                            # options=[{'label': i, 'value': i} for i in getOptions()],
-                            value='resources'
-                        ),
-                        dcc.RadioItems(
-                            id='crossfilter-xaxis-type',
-                            options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
-                            value='Linear',
-                            labelStyle={'display': 'inline-block'}
-                        )
-                    ],
-                        style={'width': '48%', 'display': 'inline-block'}),
-
-                    html.Div([
-                        dcc.Dropdown(
-                            id='crossfilter-yaxis-column',
-                            # options=[{'label': i, 'value': i} for i in getOptions()],
-                            value='sort'
-                        ),
-                        dcc.RadioItems(
-                            id='crossfilter-yaxis-type',
-                            options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
-                            value='Linear',
-                            labelStyle={'display': 'inline-block'}
-                        )
-                    ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
-                ]),
+                    dcc.Dropdown(
+                        id='x-dropdown',
+                        options=[{'label': i, 'value': i} for i in v_options],
+                        value='health'
+                    ),
+                ], style={'width': '49%', 'display': 'inline-block'}),
 
                 html.Div([
-                    dcc.Graph(
-                        id='crossfilter-indicator-scatter',
-                        hoverData={'points': [{'customdata': 'BE'}]}
-                    )
-                ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
-                html.Div([
-                    dcc.Graph(id='x-time-series'),
-                    dcc.Graph(id='y-time-series'),
-                ], style={'display': 'inline-block', 'width': '49%'}),
+                    dcc.Dropdown(
+                        id='y-dropdown',
+                        options=[{'label': i, 'value': i} for i in v_options],
+                        value='resources'
+                    ),
+                ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
+            ], style={
+                'borderBottom': 'thin lightgrey solid',
+                'backgroundColor': 'rgb(250, 250, 250)',
+                'padding': '10px 5px'
+            }),
 
-                html.Div(dcc.Slider(
-                    id='crossfilter-year--slider',
-                    min=years[0],
-                    max=years[-1],
-                    value=years[-1],
-                    marks={str(year): str(year) for year in years},
-                    step=None
-                ))
-            ])
-        ]),
+            html.Div([
+                dcc.Graph(
+                    id='crossfilter-indicator-scatter',
+                    clickData={'points': [{'hovertext': 'BEL'}]}
+                ),
+                dcc.Graph(id='cor-plot'),
+            ], style={'width': '49%', 'display': 'inline-block', 'vertical-align': 'top'}),
+
+            html.Div([
+                dcc.Graph(id='x-time-series'),
+                dcc.Graph(id='y-time-series'),
+            ], style={'display': 'inline-block', 'width': '49%'}),
+
+            html.Div(dcc.Slider(
+                id='year--slider',
+                min=years[0],
+                max=years[-1],
+                value=years[-1],
+                marks={str(year): str(year) for year in years},
+                step=None
+            ), style={'width': '49%', 'padding': '0px 20px 20px 20px'})
+        ])
     ])
 
     return layout
@@ -89,8 +78,6 @@ def getOptions(year=0):
     for item in all_data:
         if item['year'] == year:
             datas = item['datas']
-
-    # datas = session.get('datas') if session.get('datas') else []
 
     return [item['nick_name'] for item in datas if item['nick_name'] not in ['country']]
 
@@ -115,125 +102,96 @@ def updateAxiss(year):
 
 @app.callback(
     Output('crossfilter-indicator-scatter', 'figure'),
-    [Input('crossfilter-xaxis-column', 'value'),
-     Input('crossfilter-yaxis-column', 'value'),
-     Input('crossfilter-xaxis-type', 'value'),
-     Input('crossfilter-yaxis-type', 'value'), Input('crossfilter-year--slider', 'value')])
-def update_graph(xaxis_column_name, yaxis_column_name,
-                 xaxis_type, yaxis_type, year_value):
+    [Input('x-dropdown', 'value'),
+     Input('y-dropdown', 'value'),
+     Input('year--slider', 'value')])
+def update_graph(v1, v2, year_value):
     all_data = session.get('all_data')
     for item in all_data:
         if item['year'] == year_value:
-
             datas = item['datas']
             year = item['year']
             table_name = item['table_name']
-            dff = getTableDf(datas, year, table_name)[0]
-            traces = []
-            for i in dff.continent.unique():
-                df_by_continent = dff[dff['continent'] == i]
-                traces.append(dict(
-                    x=df_by_continent[df_by_continent['Indicator Name'] == xaxis_column_name]['Value'],
-                    y=df_by_continent[df_by_continent['Indicator Name'] == yaxis_column_name]['Value'],
-                    text=df_by_continent[df_by_continent['Indicator Name'] == yaxis_column_name]['Country Name'],
-                    customdata=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'],
-                    mode='markers',
-                    marker={
-                        'size': 15,
-                        'opacity': 0.5,
-                        'line': {'width': 0.5, 'color': 'white'}
-                    },
-                    name=i
-                ))
-
-            return {
-                'data': traces,
-                'layout': dict(
-                    xaxis={
-                        'title': xaxis_column_name,
-                        'type': 'linear' if xaxis_type == 'Linear' else 'log'
-                    },
-                    yaxis={
-                        'title': yaxis_column_name,
-                        'type': 'linear' if yaxis_type == 'Linear' else 'log'
-                    },
-                    margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
-                    legend={'x': 0, 'y': 1},
-                    hovermode='closest'
-                )
-            }
-
-
-def create_time_series(dff, axis_type, title):
-    return {
-        'data': [dict(
-            x=dff['Year'],
-            y=dff['Value'],
-            mode='lines+markers'
-        )],
-        'layout': {
-            'height': 225,
-            'margin': {'l': 20, 'b': 30, 'r': 10, 't': 10},
-            'annotations': [{
-                'x': 0, 'y': 0.85, 'xanchor': 'left', 'yanchor': 'bottom',
-                'xref': 'paper', 'yref': 'paper', 'showarrow': False,
-                'align': 'left', 'bgcolor': 'rgba(255, 255, 255, 0.5)',
-                'text': title
-            }],
-            'yaxis': {'type': 'linear' if axis_type == 'Linear' else 'log'},
-            'xaxis': {'showgrid': False}
-        }
-    }
+            dff = getTableDf(datas, year, table_name)[3]
+            fig = px.scatter(dff, x=v1, y=v2, color='continent', hover_name='country', marginal_y="violin",
+                             marginal_x="rug", trendline="ols")
+            return fig
 
 
 @app.callback(
     Output('x-time-series', 'figure'),
-    [Input('crossfilter-indicator-scatter', 'hoverData'),
-     Input('crossfilter-xaxis-column', 'value'),
-     Input('crossfilter-xaxis-type', 'value')])
-def update_y_timeseries(hoverData, xaxis_column_name, axis_type):
+    [Input('crossfilter-indicator-scatter', 'clickData'),
+     Input('x-dropdown', 'value')]
+)
+def update_x_timeseries(clickData, v1):
     all_data = session.get('all_data')
     df = None
-    country_name = hoverData['points'][0]['customdata']
+    country_name = clickData['points'][0]['hovertext']
 
     for item in all_data:
         datas = item['datas']
         year = item['year']
         table_name = item['table_name']
-        dfin = getTableDf(datas, year, table_name)[0]
-        dff = dfin.loc[dfin['Country Name'] == country_name]
-        dff = dff.loc[dfin['Indicator Name'] == xaxis_column_name]
+        dfin = getTableDf(datas, year, table_name)[3]
+        dff = dfin.loc[dfin['country'] == country_name]
 
         if df is None:
             df = dff
         else:
             df = df.append(dff)
 
-    return create_time_series(df, axis_type, country_name)
+    country_name = clickData['points'][0]['hovertext']
+    dff = df[df['country'] == country_name]
+    fig = px.scatter(dff, x='year', y=v1)
+    fig.data[0].update(mode='markers+lines')
+    title = '<b>{}</b><br>{}'.format(country_name, v1)
+    fig.update_layout(title_text=title)
+    return fig
 
 
 @app.callback(
     Output('y-time-series', 'figure'),
-    [Input('crossfilter-indicator-scatter', 'hoverData'),
-     Input('crossfilter-yaxis-column', 'value'),
-     Input('crossfilter-yaxis-type', 'value')])
-def update_x_timeseries(hoverData, yaxis_column_name, axis_type):
+    [Input('crossfilter-indicator-scatter', 'clickData'),
+     Input('y-dropdown', 'value')]
+)
+def update_y_timeseries(clickData, v1):
     all_data = session.get('all_data')
     df = None
-    country_name = hoverData['points'][0]['customdata']
+    country_name = clickData['points'][0]['hovertext']
 
     for item in all_data:
         datas = item['datas']
         year = item['year']
         table_name = item['table_name']
-        dfinx = getTableDf(datas, year, table_name)[0]
-        country_name = hoverData['points'][0]['customdata']
-        dff = dfinx.loc[dfinx['Country Name'] == country_name]
-        dff = dff.loc[dfinx['Indicator Name'] == yaxis_column_name]
+        dfin = getTableDf(datas, year, table_name)[3]
+        dff = dfin.loc[dfin['country'] == country_name]
 
         if df is None:
             df = dff
         else:
             df = df.append(dff)
 
-    return create_time_series(df, axis_type, country_name)
+    country_name = clickData['points'][0]['hovertext']
+    dff = df[df['country'] == country_name]
+    fig = px.scatter(dff, x='year', y=v1)
+    fig.data[0].update(mode='markers+lines')
+    title = '<b>{}</b><br>{}'.format(country_name, v1)
+    fig.update_layout(title_text=title)
+    return fig
+
+
+@app.callback(
+    Output('cor-plot', 'figure'),
+    [Input('year--slider', 'value')]
+)
+def update_graph(year_value):
+    all_data = session.get('all_data')
+    for item in all_data:
+        if item['year'] == year_value:
+            datas = item['datas']
+            year = item['year']
+            table_name = item['table_name']
+            dff = getTableDf(datas, year, table_name)[3]
+            dff.set_index('country', inplace=True)
+            fig = ff.create_scatterplotmatrix(dff[['health', 'resources']], diag='histogram', text=dff.index, height=800, width=800)
+            return fig
